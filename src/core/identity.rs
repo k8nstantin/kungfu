1d541bf323699c935b0d58f2998a6aa462901940
Constantin Alexander <constantin@dedomena.io>
2026-05-12 12:44:29 -0400

    chore: add email to copyright headers

--- src/core/identity.rs
+++ src/core/identity.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 Constantin Alexander. All rights reserved.
+// Copyright (c) 2026 Constantin Alexander <constantin@dedomena.io>. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0 that can be
 // found in the LICENSE file.
 
